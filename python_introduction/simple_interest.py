2b75f06682d33b4d10019ac8aeac18dddebd966b
Ore Victor <adesanya.ov@gmail.com>
2025-05-29 13:01:29 +0100

    Simple Interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000
-annual_interest_rate = 0.05
+rate = 0.05
 time = 3
-interest = principal * annual_interest_rate * time
-print(f"The simple interest is {interest}")
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
