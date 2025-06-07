5a01c3121edb065dbe696b7e8283cdfe9308d58a
Ore Victor <adesanya.ov@gmail.com>
2025-06-07 01:11:20 +0100

    Control Flow

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,24 @@
+task = input("Enter your task: ").lower()
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+match priority:
+    case "high":
+        message = f"Reminder: '{task}' is a {priority} priority task"
+
+    case "low":
+        message = f"Reminder: '{task}' is a {priority} priority task."
+
+    case "medium":
+        message = f"Reminder: '{task}' is a {priority} priority task."
+
+    case _:
+        message = f"Reminder: '{task}' has an unknown priority."
+
+if time_bound == "yes":
+    message += " that requires immediate attention today!"
+elif time_bound == "no":
+    message += " Consider completing it when you have free time."
+
+
+print(message)
