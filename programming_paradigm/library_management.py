a0f162dc808a5aa940084d4c8a1634297f69943b
Ore Victor <adesanya.ov@gmail.com>
2025-07-09 00:40:48 +0100

    Programming Paradigm

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,6 +4,21 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
 
 class Library:
     def __init__(self):
