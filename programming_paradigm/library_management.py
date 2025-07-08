324f06d665232619792c0290b7684e8b1261732e
Ore Victor <adesanya.ov@gmail.com>
2025-07-09 00:33:21 +0100

    Programming Paradigm

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        pass
+
+    def return_book(self, title):
+        pass
+
+    def list_available_books(self):
+        pass
+
+
